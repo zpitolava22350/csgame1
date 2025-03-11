e853ddb551ec7732a5357cd03a94d0a2848b38d6
ralarie1 <zpitolava22350@gmail.com>
2025-03-10 18:50:29 -0600

    bruh

---
+++ csgame/csgame/Block.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csgame
+{
+    class Block
+    {
+
+        private Vector3 position;
+        private Vector3 size;
+
+        public Block(Vector3 pos, Vector3 scale) {
+            position = pos;
+            size = scale;
+        }
+
+    }
+}
---
+++ csgame/csgame/World.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csgame
+{
+    class World
+    {
+
+        private List<Block> blocks = new List<Block>();
+
+        public World() {
+
+        }
+
+    }
+}
